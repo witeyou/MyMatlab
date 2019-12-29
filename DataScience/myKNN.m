3fe8d8e016ec986ca0d7a83139927366ca678699
ShiXuechao <1171838741@qq.com>
2019-12-30 00:08:01 +0800

    myKNN complete

--- DataScience/myKNN.m
+++ DataScience/myKNN.m
@@ -1,8 +1,8 @@
-% 最紧邻算法模拟
+% 最近邻算法模拟
 clc;
 clear;
 
-K = 3;    %这边用来设置K的初始值
+K = 5;    %这边用来设置K的初始值
 load('../dataSet/iris_data.mat'); %这边导入样本
 X = iris_data';
 clear iris_data;
@@ -16,12 +16,21 @@ testSet = X(:,2:2:N);
 
 % last row store the distence per time
 trainSet = cat(1,trainSet,zeros(1,length(trainSet)));
-for i = 1:1  %length(testSet)
+% last row store caculated class per simple
+testSet = cat(1,testSet,zeros(1,length(testSet)));
+for i = 1:length(testSet)
     for j = 1:length(trainSet)
         tmp = testSet(1:M,i) - trainSet(1:M,j);
         trainSet(M+2,j) = tmp'*tmp;
     end
     % the last row of trainSet is the distence between sample and trainSet
     trainSet = sortrows(trainSet',M+2)';
-    
+    classNo = mode(trainSet(M+1,1:K));
+    testSet(M+2,i) = classNo;
+    trainSet(M+2,:) = 0;
 end
+clear classNo i j tmp
+
+% effect test
+diff = find(testSet(M+1,:)-testSet(M+2,:));
+fprintf("total is %d\t mistake is %d \t correct rat is %f",length(testSet),length(diff),(1-length(diff)/length(testSet))*100)
